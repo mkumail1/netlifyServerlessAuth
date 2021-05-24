b8c0a05a3c116bf682b2516ea209c5e0e6c4b6a9
Muhammad Kumail <mkumailofficial@gmail.com>
2021-05-24 19:43:35 +0500

    auth render

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Guides.module.css";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import AuthContext from "../stores/authContext";
 
 export default function Guides() {
   const { user, authReady } = useContext(AuthContext);
+  const [guides, setGuides] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (authReady) {
@@ -15,13 +17,47 @@ export default function Guides() {
           },
         }
       )
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Wanna see this content? You must log in");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setGuides(data);
+          setError(null);
+        })
+        .catch((err) => {
+          setGuides(null);
+          setError(err.message);
+        });
     }
   }, [user, authReady]);
+
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      <h1>All Guides</h1>
+      {!authReady && <h5>Loading...</h5>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides &&
+        guides.map((guide, index) => (
+          <div key={index}>
+            <h3>{guide.title}</h3>
+            <h5>Written by {guide.author}</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem praesentium eligendi sunt eos deserunt quas saepe
+              laborum repellendus, vel id quisquam, inventore aperiam vero
+              excepturi rem ipsam? Tempore, nisi aut.
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
